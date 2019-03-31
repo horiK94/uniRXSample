536db325fee48dc200966d134ef9bcacf2fddc41
horiK <hazemi94@gmail.com>
2019-03-31 23:56:58 +0900

    カウントダウンをUniRXを用いて実装

---
+++ UniRX/Assets/TimeCounterUniRX.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class TimeCounterUniRX : MonoBehaviour
+{
+    //イベントを発行する核となるインスタンス
+    private Subject<int> timeSubject = new Subject<int>();
+
+    //イベントの購読部分だけ公開
+    public IObservable<int> OnTimeChanged
+    {
+        get
+        {
+            return timeSubject;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TimeCoroutine());
+    }
+
+    IEnumerator TimeCoroutine()
+    {
+        var time = 100;
+        while(time > 0)
+        {
+            time--;
+
+            timeSubject.OnNext(time);
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
---
+++ UniRX/Assets/TimeViewerUniRX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class TimeViewerUniRX : MonoBehaviour
+{
+    [SerializeField] private TimeCounterUniRX timeCounter = null;
+    [SerializeField] private Text counterText = null;
+
+    private void Start()
+    {
+        timeCounter.OnTimeChanged.Subscribe(_time =>
+        {
+            counterText.text = _time.ToString();
+        });
+    }
+}
