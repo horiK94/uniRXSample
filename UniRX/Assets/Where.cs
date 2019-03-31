b8e1fd06f2339b3c25d4ae8884e2b08663beda67
horiK <hazemi94@gmail.com>
2019-04-01 02:01:45 +0900

    Where関数を用いたサンプルを実装

---
+++ UniRX/Assets/Where.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class Where : MonoBehaviour
+{
+    Subject<string> subject = new Subject<string>();
+
+    private void Start()
+    {
+        subject.Where(x => x.Equals("Player"))
+            .Subscribe(col => Debug.Log(string.Format("{0}と衝突しました", col.ToString())));
+
+        subject.OnNext("Enemy");
+        subject.OnNext("Player");
+        subject.OnNext("Enemy");
+        subject.OnNext("Player");
+    }
+}
