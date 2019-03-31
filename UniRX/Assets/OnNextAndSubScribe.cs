5ca03713bb40e6d2d299101b13f3113a4f1f4f38
horiK <hazemi94@gmail.com>
2019-04-01 01:40:13 +0900

    next, SubScribe関数を用いたサンプルを実装

---
+++ UniRX/Assets/OnNextAndSubScribe.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class OnNextAndSubScribe : MonoBehaviour
+{
+    Subject<string> subject = new Subject<string>();
+
+    private void Start()
+    {
+        subject.Subscribe(msg => Debug.Log("Subscribe1" + msg));
+        subject.Subscribe(msg => Debug.Log("Subscribe2" + msg));
+        subject.Subscribe(msg => Debug.Log("Subscribe3" + msg));
+
+        subject.OnNext("おはようから");       //登録されt関数をすべて行う
+        subject.OnNext("お休みまで");
+    }
+}
