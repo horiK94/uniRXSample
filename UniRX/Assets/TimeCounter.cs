7b7ca7a9ae603a767547755a829ffa5eb5713392
horiK <hazemi94@gmail.com>
2019-03-31 12:23:31 +0900

    eventを用いたコードを実装

---
+++ UniRX/Assets/TimeCounter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeCounter : MonoBehaviour
+{
+    //C言語で言うところの関数ポインタ
+    /// <summary>
+    /// イベントハンドラ(イベントメッセージの型定義)
+    /// </summary>
+    /// <param name="time"></param>
+    public delegate void TimeEventHandler(int time);
+
+    /*
+     * デリゲートに対する「プロパティ」みたいなもの
+     * ・デリゲート呼び出しはクラスの内部からのみ可能
+     * 外部からはデリゲートの追加/
+     * */
+    /// <summary>
+    /// 
+    /// </summary>
+    public event TimeEventHandler OnTimeChanged;
+
+    private void Start()
+    {
+        StartCoroutine(TimeCountinue());
+    }
+
+    //何をするか決める関数部分を外部に決めてもらい、いつその関数を呼ぶかはeventを作成した側が呼ぶ
+    /// <summary>
+    /// カウントダウン
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator TimeCountinue()
+    {
+        var time = 100;
+        while(time > 0)
+        {
+            time--;
+            OnTimeChanged(time);
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
---
+++ UniRX/Assets/TimeViewer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeViewer : MonoBehaviour
+{
+    [SerializeField] private TimeCounter timeCounter = null;
+    [SerializeField] private Text text = null;
+
+    private void Start()
+    {
+        timeCounter.OnTimeChanged += (time) =>
+        {
+            text.text = time.ToString();
+        };
+    }
+}
